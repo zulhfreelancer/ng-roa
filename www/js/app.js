b47a0d39fa34bef39a7b2a32fdac5e84967a8a16
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-06-20 15:44:48 +0800

    Add some data to view

--- www/js/app.js
+++ www/js/app.js
@@ -1,71 +1,66 @@
-// Ionic Starter App
+var app = angular.module('ionicApp', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+app.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+  $ionicConfigProvider.views.maxCache(0);
 
-.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
+    
+    .state('/', {
+      url: '/',
+      controller: 'HomeCtrl',
+      templateUrl: 'menu.html'
+    });
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
+  $urlRouterProvider.otherwise('/');
+});
 
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
+app.controller('HomeCtrl', function($scope) {
+
+  $scope.selected = false;
+
+  $scope.getMenus = function(a){
+    
+    var data = {
+                   "foods":[
+                      {
+                          "name":"Nasi Goreng",
+                          "qty":null
+                      },
+                      {
+                          "name":"Rojak Ayam",
+                          "qty":null
+                      }
+                   ],
+                   "drinks":[
+                      {
+                          "name":"Milo Ais",
+                          "qty":null
+                      },
+                      {
+                          "name":"Sirap Ais",
+                          "qty":null
+                      }
+                   ]
+                  };
+
+    if (a == "foods") {
+      var foodsIsSelected = 1;
+      $scope.data = data.foods;
+    } else {
+      var drinksIsSelected = 1;
+      $scope.data = data.drinks;
     }
-  })
 
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
+    $scope.isActive = function(b) {
+      console.log(b);
+      if ((foodsIsSelected == 1) && (b == 'foods')) {
+        $scope.selected = true;
+      } else if ((drinksIsSelected == 1) && (b == 'drinks')) {
+        $scope.selected = true;
       }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+    };
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  }; // end scope getMenus
+
 });
