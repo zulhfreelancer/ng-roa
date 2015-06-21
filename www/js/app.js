dbee7268b16ae5c7fa0b18e1b99516f3ae6d8e0c
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-06-22 06:54:19 +0800

    Create deleteLocalStorage service & add Delete button in kitchen view

--- www/js/app.js
+++ www/js/app.js
@@ -2,8 +2,6 @@ var app = angular.module('myApp', ['ngStorage']);
 
 app.controller('tableCtrl', function($scope, $localStorage) {
 
-	
-
 	$(document).ready(function() {
         $("#showMenu").hide(function() {});
     });
@@ -77,18 +75,30 @@ app.controller('tableCtrl', function($scope, $localStorage) {
 
 });
 
-app.controller('successCtrl', function($scope, $localStorage) {
+app.controller('successCtrl', function($scope, $localStorage, deleteLocalStorage) {
 
-	
+    $scope.localStorageFoods    = $localStorage.foods;
+    $scope.localStorageDrinks   = $localStorage.drinks;
+    $scope.table_number         = $localStorage.table_number;
 
-    $scope.localStorageFoods = $localStorage.foods;
-    $scope.localStorageDrinks = $localStorage.drinks;
-    $scope.table_number = $localStorage.table_number;
-
-    
-
-    //delete $localStorage.foods;
-    //delete $localStorage.drinks;
-    //delete $localStorage.table_number;
+    // Delete localStorage data if the button inside kitchen view is clicked
+    $scope.deleteAll = function(){
+        // Delegate the job to factory
+        deleteLocalStorage.deleteAll();
+        $scope.localStorageFoods    = "";
+        $scope.localStorageDrinks   = "";
+        $scope.table_number         = "";
+    };
 
 });
+
+app.factory('deleteLocalStorage',  ['$localStorage', function ($localStorage) {
+    return {
+        deleteAll: function() {
+            delete $localStorage.foods;
+            delete $localStorage.drinks;
+            delete $localStorage.table_number;
+            alert('Order deleted');
+        }
+    };
+}]);
